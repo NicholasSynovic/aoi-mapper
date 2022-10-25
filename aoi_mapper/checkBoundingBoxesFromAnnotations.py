7112567d149072fc5fcbaa6c64d2971fdcc7ca9a
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-10-25 17:33:31 -0500

    Implement better methods for exporting images

--- aoi_mapper/checkBoundingBoxesFromAnnotations.py
+++ aoi_mapper/checkBoundingBoxesFromAnnotations.py
@@ -1,69 +1,152 @@
-from collections import namedtuple
 from json import load
 
 import cv2
 from lxml.etree import _Element, _ElementTree, parse
-from numpy import array, ndarray
-from PIL import Image
+from numpy import ndarray
 from progress.bar import Bar
+import numpy
+from pathlib import PurePath
 
-Corners = namedtuple("Corners", ["xmin", "ymin", "xmax", "ymax"], defaults=[0, 0, 0, 0])
+blackWhiteThreshold: int = 128
 
+def loadJSON(filepath: PurePath)    ->  dict:
+    data: dict = {}
 
-def main() -> None:
-    with open("annotationMap.json", "r") as jsonFile:
-        data: dict = load(jsonFile)
+    with open(filepath, "r") as jsonFile:
+        fileData: dict = load(jsonFile)
         jsonFile.close()
 
-    topLevelKeys: list = data.keys()
+    topLevelKeys: list = fileData.keys()
 
-    with Bar("Checking bounding boxes for each type of aoi map...", max=10) as bar:
-        tlk: str
-        for tlk in topLevelKeys:
-            xmlFilePath: str
-            for xmlFilePath in data[tlk].keys():
-                try:
-                    et: _ElementTree = parse(xmlFilePath)
-                except OSError:
-                    continue
+    tlk: str
+    for tlk in topLevelKeys:
+        data.update(fileData[tlk])
 
-                objectElement: _Element = et.find(path="object")
-                bndBoxElement: _Element = objectElement.find(path="bndbox")
+    return data
 
-                coordinates: dict = {}
-                idx: int
-                for idx in range(len(bndBoxElement)):
-                    match bndBoxElement[idx].tag:
-                        case "xmin":
-                            coordinates["xmin"] = float(bndBoxElement[idx].text)
-                        case "ymin":
-                            coordinates["ymin"] = float(bndBoxElement[idx].text)
-                        case "xmax":
-                            coordinates["xmax"] = float(bndBoxElement[idx].text)
-                        case "ymax":
-                            coordinates["ymax"] = float(bndBoxElement[idx].text)
-                        case _:
-                            print("ERROR", xmlFilePath)
+def loadBBoxFromFile(filepath: str)    ->  dict | None:
+    try:
+        et: _ElementTree = parse(filepath)
+    except OSError:
+        return None
 
-                topLeft: tuple = (
-                    round(coordinates["xmin"]),
-                    round(coordinates["ymin"]),
-                )
-                bottomRight: tuple = (
-                    round(coordinates["xmax"]),
-                    round(coordinates["ymax"]),
-                )
+    objectElement: _Element = et.find(path="object")
+    bndBoxElement: _Element = objectElement.find(path="bndbox")
 
-                imageFilePath: str = data[tlk][xmlFilePath]
-                image: ndarray = array(Image.open(imageFilePath).convert("RGB"))
-                cv2.rectangle(
-                    image, topLeft, bottomRight, color=(255, 0, 0), thickness=1
-                )
-                cv2.imshow("dark", image)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+    coordinates: dict = {}
+    idx: int
+    for idx in range(len(bndBoxElement)):
+        match bndBoxElement[idx].tag:
+            case "xmin":
+                coordinates["xmin"] = float(bndBoxElement[idx].text)
+            case "ymin":
+                coordinates["ymin"] = float(bndBoxElement[idx].text)
+            case "xmax":
+                coordinates["xmax"] = float(bndBoxElement[idx].text)
+            case "ymax":
+                coordinates["ymax"] = float(bndBoxElement[idx].text)
+            case _:
+                print("ERROR LOADING BBOX COORDINATES:", filepath)
 
-            bar.next()
+    return coordinates
+
+def main() -> None:
+    annotationsMap: PurePath = PurePath("annotationMap.json")
+    data: dict = loadJSON(filepath=annotationsMap)
+
+    with Bar("Checking bounding boxes for each type of aoi map...", max=len(data.keys())) as bar:
+        xmlFilePath: str
+        for xmlFilePath in data.keys():
+            coordinates: dict | None = loadBBoxFromFile(filepath=xmlFilePath)
+
+            if coordinates is None:
+                bar.next()
+                continue
+
+            topLeft: tuple = (
+                round(coordinates["xmin"]),
+                round(coordinates["ymin"]),
+            )
+            bottomRight: tuple = (
+                round(coordinates["xmax"]),
+                round(coordinates["ymax"]),
+            )
+
+            imageFilePath: str = data[xmlFilePath]
+
+            image: ndarray = cv2.imread(imageFilePath)
+
+            imageArea: int = image.shape[0] * image.shape[1]
+            whitePixels: int = numpy.sum(image >= 128) / 3
+            blackPixels: int = numpy.sum(image < 128) / 3
+            pixelSum = blackPixels + whitePixels
+            error = pixelSum - imageArea
+
+            otherPixels: int = numpy.sum((image != 0) & (image != 255))
+
+            print()
+            print(imageFilePath)
+            print("black pixels:", blackPixels)
+            print("white pixels:",whitePixels)
+            print()
+            print("pixel sum:",pixelSum)
+            print("area:", imageArea)
+            print("actual:", 500 * 343)
+            print("Error:", error)
+            print("other pixels:", otherPixels)
+
+            imageUniquePixelValues = numpy.unique(image)
+            print(imageUniquePixelValues)
+
+            # hist = numpy.histogram(temp)
+            # print(hist)
+
+
+
+            # wrongPixels: ndarray = numpy.where((image != 0) & (image != 255))
+            # print(wrongPixels)
+            # # shape: tuple = image.shape
+
+            # totalBlackPixels: int = numpy.sum(image == 255) / 3
+            # totalWhitePixels: int = numpy.sum(image == 0) / 3
+
+            # totalPixels: int = shape[0] * shape[1]
+
+            # print()
+            # print("total bp:", totalBlackPixels)
+            # print("total wp:", totalWhitePixels)
+            # print("bp + wp:", totalBlackPixels + totalWhitePixels)
+            # print("total p:", totalPixels)
+
+            # print("error:", totalBlackPixels + totalWhitePixels - totalPixels)
+
+
+            # image = image[
+            #     round(coordinates["ymin"]) : round(coordinates["ymax"]),
+            #     round(coordinates["xmin"]) : round(coordinates["xmax"]),
+            # ]
+
+            # totalBlackPixels: int = numpy.sum(image == 255) / 3
+            # totalWhitePixels: int = numpy.sum(image == 0) / 3
+            # totalPixels: int = (totalBlackPixels + totalWhitePixels) / 3
+
+            # print(
+            #     "\n",
+            #     totalBlackPixels,
+            #     totalWhitePixels,
+            #     totalPixels,
+            # )
+
+            # # image: ndarray = array(Image.open(imageFilePath).convert("RGB"))
+            # # cv2.rectangle(
+            # #     image, topLeft, bottomRight, color=(255, 0, 0), thickness=1
+            # # )
+            # cv2.imshow(xmlFilePath, image)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            quit()
+
+        bar.next()
 
 
 if __name__ == "__main__":
