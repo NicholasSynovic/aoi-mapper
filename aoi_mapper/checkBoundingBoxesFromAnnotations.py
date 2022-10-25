b94fca25614d0ee9b71df9a96a5b391c531094f7
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-10-25 14:31:11 -0500

    Add code to place a bounding box around the proper area

--- aoi_mapper/checkBoundingBoxesFromAnnotations.py
+++ aoi_mapper/checkBoundingBoxesFromAnnotations.py
@@ -1,10 +1,13 @@
 from collections import namedtuple
 from json import load
 
+import cv2
 from lxml.etree import _Element, _ElementTree, parse
+from numpy import array, ndarray
+from PIL import Image
 from progress.bar import Bar
 
-Corners = namedtuple("Corners", ["xmin", "ymin", "xmax", "ymax"])
+Corners = namedtuple("Corners", ["xmin", "ymin", "xmax", "ymax"], defaults=[0, 0, 0, 0])
 
 
 def main() -> None:
@@ -27,14 +30,38 @@ def main() -> None:
                 objectElement: _Element = et.find(path="object")
                 bndBoxElement: _Element = objectElement.find(path="bndbox")
 
-                coordinates: Corners = Corners(
-                    bndBoxElement[0].text,
-                    bndBoxElement[1].text,
-                    bndBoxElement[2].text,
-                    bndBoxElement[3].text,
+                coordinates: dict = {}
+                idx: int
+                for idx in range(len(bndBoxElement)):
+                    match bndBoxElement[idx].tag:
+                        case "xmin":
+                            coordinates["xmin"] = float(bndBoxElement[idx].text)
+                        case "ymin":
+                            coordinates["ymin"] = float(bndBoxElement[idx].text)
+                        case "xmax":
+                            coordinates["xmax"] = float(bndBoxElement[idx].text)
+                        case "ymax":
+                            coordinates["ymax"] = float(bndBoxElement[idx].text)
+                        case _:
+                            print("ERROR", xmlFilePath)
+
+                topLeft: tuple = (
+                    round(coordinates["xmin"]),
+                    round(coordinates["ymin"]),
+                )
+                bottomRight: tuple = (
+                    round(coordinates["xmax"]),
+                    round(coordinates["ymax"]),
                 )
 
-                quit()
+                imageFilePath: str = data[tlk][xmlFilePath]
+                image: ndarray = array(Image.open(imageFilePath).convert("RGB"))
+                cv2.rectangle(
+                    image, topLeft, bottomRight, color=(255, 0, 0), thickness=1
+                )
+                cv2.imshow("dark", image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
 
             bar.next()
 
